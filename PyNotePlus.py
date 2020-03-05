42d51d3c4c88ea0c3078e25c28672391cfbe478a
HT_NDUJMOVIC <Neven.Dujmovic@t.ht.hr>
2020-03-05 16:40:43 +0100

    working windows version, added comments

--- PyNotePlus.py
+++ PyNotePlus.py
@@ -17,24 +17,25 @@ from PIL import ImageTk, ImageGrab  # for Windows environment
 
 # region ========== Global variables & settings ==========
 
-# initialize tkinter, with creation of Tk root widget (basic window, title bar as defined by OS window manager)
+# # initialize tkinter, with creation of Tk root widget (basic window, title bar as defined by OS window manager)
 root = Tk()
 
-# add default font, title and geometry
+# # set default font attributes and assign it to global object "font_style"
 font_style = tk_font.Font(family="Courier New", size=10)
+# # set title
 root.title("Process text (developed by Neven Dujmovic)")
-# (width x height + x_offset + y_offset)
+# # set geometry (width x height + x_offset + y_offset)
 root.geometry("820x400+300+200")
 
-# in case you want to make form not resizable just uncomment line below
+# # in case you want to make form not resizable just uncomment line below
 # form.resizable(0, 0)
 
-# place tkinter window on top of the others
+# # place tkinter window on top of the others
 root.attributes('-topmost', True)
 root.update()
 
-# lets make TAB control by using tkinter.Notebook widget (https://wiki.tcl-lang.org/page/tkinter.Notebook)
-# widget manages a collection of child windows and displays a single one at a time..
+# # lets make TAB control by using tkinter.Notebook widget (https://wiki.tcl-lang.org/page/tkinter.Notebook)
+# # widget manages a collection of child windows and displays a single one at a time..
 tab_parent = ttk.Notebook(root)
 
 # endregion
@@ -42,41 +43,41 @@ tab_parent = ttk.Notebook(root)
 
 # region =================== Tab 1 =======================
 
-# this function is used to open text file and load its content to text box widget
+# # this function is used to open text file and load its content to text box widget
 def open_text_file():
-    # ensure error handling in case text file is NOT selected
+    # # ensure error handling in case text file is NOT selected
     try:
-        # open select file dialog and assign full path to variable name
+        # # open select file dialog and assign full path to variable name
         name = fd.askopenfilename()
-
-        # open text file for reading
+        # # open text file for reading
         f = open(name, "r")
-
-        # all lines in the file with readlines
-        # The variable "lines" is a list containing all lines in the file
+        # # read all lines in the file with method "readlines"
+        # # variable "lines" is a list that will contain all lines in the file
         lines = f.readlines()
         txt_main.insert(tk.END, lines)
-
-        # close the file after reading the lines.
+        # # close the file after reading the lines.
         f.close()
-        # just for the test - print selected file path in console
+        # # just for the test - print selected file path in console
         print(name)
-    except:
+    except:  # # for now you can just ignore "PEP 8: do not use bare 'except' - Too broad exception clause"
+        # # in case of error show message with tkinter "messagebox" object
         messagebox.showinfo(message="Text file not selected.")
 
 
+# # every call of the function will increase font size by 2 in the txt_main object (of tk.Text type)
 def increase_text_font():
+    # # assign "size" attribute to a variable
     font_size = font_style['size']
+    # # set new "size" to global object "font_style"
     font_style.configure(size=font_size + 2)
 
 
+# # every call of the function will decrease font size by 2 in the txt_main object (of tk.Text type)
 def decrease_text_font():
+    # # assign "size" attribute to a variable
     font_size = font_style['size']
+    # # set new "size" to global object "font_style"
     font_style.configure(size=font_size - 2)
-
-
-def process_text():
-    pass
 
 
 tab1 = ttk.Frame(tab_parent)
